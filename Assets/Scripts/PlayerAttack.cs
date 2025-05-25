68f50b503f956b255a5c9c0be7bfa32f70501457
Kurt Amodia <kaamodia@up.edu.ph>
2025-05-25 20:56:38 +0800

    Added assets for Laser Weapon

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] private GameObject defaultAttackPfx;
-    [SerializeField] private float attackDistance = 0.5f;
+    // [SerializeField] private float attackDistance = 0.5f;
     [SerializeField] private float attackDuration = 0.25f;
     [SerializeField] private float attackCooldown = 1f;
 
