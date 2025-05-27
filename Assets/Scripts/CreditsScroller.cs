6dd65de1828efc68882695500038fccee84bbcaf
Rainer <rtmayagma@up.edu.ph>
2025-05-27 23:54:33 +0800

    credits scene

---
+++ Assets/Scripts/CreditsScroller.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CreditsScroller : MonoBehaviour
+{
+    [SerializeField] private float scrollSpeed = 50f;
+    [SerializeField] private float returnDelay = 20f; // Time before returning to MainMenu
+    [SerializeField] private string mainMenuSceneName = "MainMenu"; // Make sure this matches your scene name
+
+    private RectTransform rectTransform;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        StartCoroutine(ReturnToMainMenuAfterDelay());
+    }
+
+    void Update()
+    {
+        rectTransform.anchoredPosition += new Vector2(0, scrollSpeed * Time.deltaTime);
+    }
+
+    private IEnumerator ReturnToMainMenuAfterDelay()
+    {
+        yield return new WaitForSeconds(returnDelay);
+        SceneManager.LoadScene(mainMenuSceneName);
+    }
+}
